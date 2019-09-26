f59ba5f9d95e142ed2ee3be02be4adc66de7680d
tomu <kusano.wireless@gmail.com>
2019-09-26 13:59:11 +0000

    bug fix

--- variables.tf
+++ variables.tf
@@ -12,15 +12,15 @@ variable "resource_group_name" {
 
 variable "platform_update_domain_count" {
   description = "(Optional) Specifies the number of update domains that are used. Defaults to 5."
-  defaults    = "5"
+  default    = "5"
 }
 
 variable "platform_fault_domain_count" {
   description = "(Optional) Specifies the number of fault domains that are used. Defaults to 3."
-  defaults    = "3"
+  default    = "3"
 }
 
 variable "managed" {
   description = "(Optional) Specifies whether the availability set is managed or not. Possible values are true (to specify aligned) or false (to specify classic). Default is true."
-  defaults    = "true"
+  default    = "true"
 }
