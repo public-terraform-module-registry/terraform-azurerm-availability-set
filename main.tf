0db121187276b19c538677dc018d049f2f55c715
tomu <kusano.wireless@gmail.com>
2019-09-27 08:44:55 +0000

    bug fix

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ resource "azurerm_availability_set" "availability_set" {
   name                         = "${var.name}"
   platform_update_domain_count = "${var.platform_update_domain_count}"
   platform_fault_domain_count  = "${var.platform_fault_domain_count}"
-  resource_group_name          = "${var.res_group_name}"
+  resource_group_name          = "${var.resource_group_name}"
   managed                      = "${var.managed}"
 }
